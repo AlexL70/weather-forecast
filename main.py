bce528d5b4036e6f9dcb58985da3e2bc08970b45
Alexander Levinson <alexander.levinson.70@gmail.com>
2024-10-22 14:39:54 +0300

    290. Add Sky Conditions

--- main.py
+++ main.py
@@ -3,6 +3,20 @@ import plotly.express as px
 from web_client import get_data, DataKey
 
 
+class SkyKeys:
+    SKY_CLEAR = "Clear"
+    SKY_CLOUDS = "Clouds"
+    SKY_RAIN = "Rain"
+    SKY_SNOW = "Snow"
+
+
+sky_dict = {
+    SkyKeys.SKY_CLEAR: "images/clear.png",
+    SkyKeys.SKY_CLOUDS: "images/cloud.png",
+    SkyKeys.SKY_RAIN: "images/rain.png",
+    SkyKeys.SKY_SNOW: "images/snow.png"
+}
+
 st.title("Weather Forecast for the Next 5 Days")
 place = st.text_input("Place:", placeholder="The name of the place")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
@@ -10,25 +24,44 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
 option = st.selectbox("Select data to view:",
                       (DataKey.DK_TEMPERATURE, DataKey.DK_SKY))
 
-if len(place) > 0:
+if place:
     if option == DataKey.DK_TEMPERATURE:
         st.subheader(f"Temperature for the next {days} days in {place}")
     else:
         st.subheader(f"Sky condition for the next {days} days in {place}")
-    success, data = get_data(place, days, option)
+    success, data = get_data(place)
     if success:
-        x_data = [item["date"] for item in data]
+        filtered = data["list"][:days*8]
         match option:
             case DataKey.DK_TEMPERATURE:
-                y_data = [item["temperature"] for item in data]
+                filtered = [{"date": item["dt_txt"],
+                             "temperature": item["main"]["temp"]} for item in filtered]
+                date_data = [item["date"] for item in filtered]
+                sky_data = [item["temperature"] for item in filtered]
                 label_y = "Temperature(C)"
-                figure = px.line(x=x_data, y=y_data,
+                figure = px.line(x=date_data, y=sky_data,
                                  labels={"x": "Date", "y": label_y})
                 st.plotly_chart(figure)
             case DataKey.DK_SKY:
-                y_data = [item["sky"] for item in data]
+                filtered = [{"date": item["dt_txt"],
+                             "sky": item["weather"][0]["main"]} for item in filtered]
+                date_data = [item["date"] for item in filtered]
+                sky_data = [sky_dict[item["sky"]] for item in filtered]
                 label_y = "Sky"
-                st.warning("The sky condition is under construction")
+                col1, col2, col3, col4 = st.columns(4)
+                for index in range(0, len(sky_data), 4):
+                    with col1:
+                        st.image(sky_data[index],
+                                 caption=date_data[index], width=120)
+                    with col2:
+                        st.image(sky_data[index+1],
+                                 caption=date_data[index+1], width=120)
+                    with col3:
+                        st.image(sky_data[index+2],
+                                 caption=date_data[index+2], width=120)
+                    with col4:
+                        st.image(sky_data[index+3],
+                                 caption=date_data[index+3], width=120)
 
     else:
         st.error(data["message"])
--- web_client.py
+++ web_client.py
@@ -10,26 +10,13 @@ class DataKey:
     DK_SKY = "Sky"
 
 
-def get_data(place: str, forcast_days: int = 1, kind: str | None = None) -> tuple[bool, dict]:
+def get_data(place: str) -> tuple[bool, dict]:
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&units=metric&appid={API_KEY}"
     response: rq.Response = rq.get(url)
     content = response.json()
     if not response.ok:
         return False, content
-    print(len(content['list']))
-    filtered = content["list"][:forcast_days*8]
-    match kind:
-        case DataKey.DK_TEMPERATURE:
-            filtered = [{"date": item["dt_txt"],
-                         "temperature": item["main"]["temp"]} for item in filtered]
-        case DataKey.DK_SKY:
-            filtered = [{"date": item["dt_txt"],
-                         "sky": item["weather"][0]["main"]} for item in filtered]
-        case _:
-            return False, {"message": f"Invalid data key: {kind}"}
-    print(filtered)
-    print(len(filtered))
-    return True, filtered
+    return True, content
 
 
 if __name__ == "__main__":
